e5f4a73082db1d25c458244c76843f3dd89fcb89
Eric Long <eric.n.long@gmail.com>
2023-02-21 12:07:12 -0800

    Add a comment

--- projects/helloworld/myfirstprogram.cpp
+++ projects/helloworld/myfirstprogram.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main() {
-  cout << "Hello World!\n\n";
+  cout << "Hello World!"\n\n; //This is the first line seperating from the second by a blank line.
   cout << "I am learning C++"; 
   return 0;
 }
